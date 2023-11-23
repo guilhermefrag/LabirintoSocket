170180a193f60c4c3109d5134054424621199a61
Guilherme Fragnani <guilhermefragnani@icloud.com>
2023-11-22 23:38:41 -0300

    add botao de sair, so falta tratar as exceptions ou algo assim

--- src/LabirintoClientSocket.java
+++ src/LabirintoClientSocket.java
@@ -64,17 +64,15 @@ public class LabirintoClientSocket {
                 currentLevel.printLevel();
                 currentLevel.printSuccessArt();
                 currentLevel.printFinalArt();
-
+                currentLevel.printRestartArt();
                 System.out.println("Você completou todos os níveis. Deseja sair? (S para sair, qualquer outra tecla para continuar): ");
                 userInput = scanner.nextLine();
-
+                if (!userInput.equalsIgnoreCase("S")) {
+                    currentLevel.printRestartArt();
                 if (userInput.equalsIgnoreCase("S")) {
                     System.out.println("Obrigado por jogar! Até logo!");
                     clientSocket.close();
                     System.exit(0);
-                }else{
-                    currentLevel.printRestartArt();
-                }
             }
             else if (isLevelCompleted) {
                 currentLevel.printLevel();
@@ -84,6 +82,8 @@ public class LabirintoClientSocket {
                 currentLevel.printFailedArt();
             }
 
-        }
+
     }
-}}
+}
+        }
+    }}
